0c73723537400f39ce468ecc38adf70fcde21fc6
FruZ333 <vasabadik@gmail.com>
2026-09-16 20:48:43 +0600

    simple connection

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
-	"study/Study2/feature1"
+	"study/feature1"
+	simpleconnection "study/feature_postgres/simple_connection"
 )
 
 func main() {
 	fmt.Println("Hello, Git!")
 	feature1.Feature1()
+
+	simpleconnection.CheckConnection()
 }
