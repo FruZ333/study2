7f7978f3ffac1977b3dd68ca2a6c06b018d0d16b
FruZ333 <vasabadik@gmail.com>
2026-09-16 21:13:10 +0600

    feature/postgres

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"study2/feature1"
 	"study2/feature2"
+	simpleconnection "study2/feature_postgres/simple_connection"
 )
 
 func main() {
@@ -12,4 +13,6 @@ func main() {
 	feature1.Feature1()
 
 	feature2.Feature2()
+
+	simpleconnection.SimpleConnection()
 }
